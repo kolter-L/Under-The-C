1a18ecaf05b25e3b5e056e19abeb14345d305f65
Kolter Leisy <kolterleisy@Kolters-MacBook-Pro.local>
2025-03-31 00:31:39 -0700

    Finishing TicTacToe

---
+++ EasyC/tictac.c
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_board(int x, int y, char board[x][y]){
+    printf("-------------\n");
+    for (int i=0; i<x; i++){
+        for (int j=0; j<y; j++){
+            printf("| %c ", board[i][j]);
+        }
+        printf("|\n");
+        printf("-------------\n");
+    }
+}
+
+int* next_move() {
+    int* res = malloc(2*sizeof(int));
+    res[0]=-3;
+    res[1]=-3;
+
+    while(!(res[0]>-1) || !(res[0]<3)) {
+        printf("Enter Row: ");
+        scanf("%d", &res[0]);
+    }
+
+    while(!(res[1]>-1) || !(res[1]<3)) {
+        printf("Enter Col: ");
+        scanf("%d", &res[1]);
+    }
+
+    return res;
+}
+
+
+int game_state(int x, int y, char board[3][3]) {
+    int state = 0;
+    // ROWS AND COLS
+    for (int i=0; i<3; i++) {
+        if (board[i][0] != ' ' && board[i][0]==board[i][1] && board[i][1]==board[i][2]) {
+            if (board[i][0]=='X') {
+                state = 1;
+            }
+            else {
+                state = -1;
+            }
+        }
+        if (board[0][i] != ' ' && board[0][i]==board[1][i] && board[1][i]==board[2][i]) {
+            if (board[0][i]=='X') {
+                state = 1;
+            }
+            else {
+                state = -1;
+            }
+        }
+    }
+    if (board[0][0] != ' ' && board[0][0]==board[1][1] && board[1][1]==board[2][2]) {
+        if (board[0][0]=='X') {
+            state = 1;
+        }
+        else {
+            state = -1;
+        }
+    }
+    if (board[2][0] != ' ' && board[2][0]==board[1][1] && board[1][1]==board[0][2]) {
+        if (board[2][0]=='X') {
+            state = 1;
+        }
+        else {
+            state = -1;
+        }
+    }
+    return state;
+}
+
+
+int main() {
+    int state = 0;
+    
+    char board[3][3] = {
+        {' ', ' ', ' '},
+        {' ', ' ', ' '},
+        {' ', ' ', ' '}
+    };
+
+    int index=0;
+    while (state==0 && index<10) {
+        print_board(3, 3, board);
+        
+        if (index%2==0){
+            printf("X Turn\n");
+            int* next = next_move();
+            board[next[0]][next[1]]='X';
+        }
+        else{
+            printf("O Turn\n");
+            int* next = next_move();
+            board[next[0]][next[1]]='O';
+        }
+        state = game_state(3, 3, board);
+        index++;
+    }
+
+    print_board(3, 3, board);
+    if (state == 1){
+        printf("X Wins");
+    }
+    else {
+        printf("O wins");
+    }
+    return 0;
+}
